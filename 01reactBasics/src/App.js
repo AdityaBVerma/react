f8bfc2af4704cfcaa23656c624d89f99726700bf
AdityaBVerma <adityabverma1@gmail.com>
2024-09-12 23:05:26 +0530

    starting of react journey

---
+++ 01reactBasics/src/App.js
@@ -0,0 +1,22 @@
+
+function App() {
+  document.body.style.backgroundColor = '#11101e'
+  const divStyle = {
+    backgroundColor: 'lightblue',
+    padding: '20px',
+    textAlign: 'center'
+  };
+  const headingStyle = {
+    color: 'darkblue',
+    fontSize: '24px'
+  };
+  return (
+    <>
+    <body style={divStyle}>
+      <h1 style={headingStyle}>Jay Ganesh || React start</h1>
+    </body>
+    </>
+  );
+}
+
+export default App;
