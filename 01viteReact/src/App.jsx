7170ade450bcf950fae1a97bfcefdfaae1e9e2f1
AdityaBVerma <adityabverma1@gmail.com>
2024-09-12 23:39:57 +0530

    react app bundled with vite

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  const headingStyle =  {
+    color: 'white'
+  }
+
+  return (
+    <>
+      <h1 style={headingStyle}>Hi this is a react app bundled with vite!!</h1>
+    </>
+  )
+}
+
+export default App
