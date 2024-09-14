6d1ebb23cbf83f21e0397a1f2ad09d1a731a23c4
AdityaBVerma <adityabverma1@gmail.com>
2024-09-14 15:36:20 +0530

    building custom react components

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -3,10 +3,10 @@ function App() {
   const headingStyle =  {
     color: 'white'
   }
-
+  const evaluatedValue = "hello"
   return (
     <>
-      <h1 style={headingStyle}>Hi this is a react app bundled with vite!!</h1>
+      <h1 style={headingStyle}>{evaluatedValue? "hi":"hello" } Hi this is a react app bundled with vite!!</h1>
     </>
   )
 }
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+function HelloButton(){
+  return (
+    <button>Press to say hi !!</button>
+  )
+}
+
+const customReactElement = {
+  type: "a",
+  props: {
+      href : "https://www.google.com/",
+      target: "_blank"
+  },
+  children : "This is a custom react component"
+}
+
+const evaluatedValue = "this is a evaluated value"
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href : "https://www.google.com/",
+    target: "_blank"
+  },
+  "this is a react element",
+  evaluatedValue
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  //reactElement
+    // HelloButton()
+  <App />
 )
