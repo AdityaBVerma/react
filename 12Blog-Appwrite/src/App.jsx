1d017e619c45e72f3c9d81cf732c770bbbe1a518
AdityaBVerma <adityabverma1@gmail.com>
2024-10-05 23:20:47 +0530

    project setup and dependency installation

---
+++ 12Blog-Appwrite/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h4>Jay Ganesh</h4>
+      <h1>This is a blogging platform made with appwrite</h1>
+    </>
+  )
+}
+
+export default App
