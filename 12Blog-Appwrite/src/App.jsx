c7b7be29ba5e3f31346fb976e62736f898d7f4f4
AdityaBVerma <adityabverma1@gmail.com>
2024-10-06 23:22:36 +0530

    configuring store and authSlice

--- 12Blog-Appwrite/src/App.jsx
+++ 12Blog-Appwrite/src/App.jsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import authService from './appwrite/auth'
+import { useDispatch } from 'react-redux'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components/index.js'
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+
+  return !loading ? (
     <>
+    <Header />
       <h4>Jay Ganesh</h4>
       <h1>This is a blogging platform made with appwrite</h1>
+      <main>
+        {/* <Outlet /> */}
+      </main>
+    <Footer />
     </>
-  )
+  ) : null
 }
 
 export default App
---
+++ 12Blog-Appwrite/src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header () {
+    return (
+        <>
+            <h1>this is a header component</h1>
+        </>
+    )
+}
