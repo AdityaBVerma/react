6a3c6ac939a9d9f700e5af1e8d163f539e495192
AdityaBVerma <adityabverma1@gmail.com>
2024-10-12 11:47:51 +0530

    Login and Signup component created

--- 12Blog-Appwrite/src/components/index.js
+++ 12Blog-Appwrite/src/components/index.js
@@ -1,7 +1,25 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button"
+import Input from "./Input"
+import Login from "./Login";
+import Logo from "./Logo";
+import PostCard from "./PostCard"
+import Select from "./Select"
+import Signup from "./Signup";
 
 export {
     Header,
     Footer,
+    Container,
+    LogoutBtn,
+    Button,
+    Input,
+    Login,
+    Logo,
+    PostCard,
+    Select,
+    Signup
 }
